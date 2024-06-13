d4f9d1dc70c46f27410cc4d27b2d0e4f3946c38a
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-06-13 10:29:32 +0330

    making the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Calender from './components/calender';
 import './index.css';
 
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Calender />}></Route>));
+
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
